63abe1963036f05a531e51614d898124c5933a12
Al Hasanul Banna <alhasanulbanna@gmail.com>
2024-05-21 00:44:51 +0600

    banner added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,9 +1,42 @@
+import { Container } from "react-bootstrap";
 import "./Banner.css";
+import bannerImg from "../../../assets/images/banner/banner-img.png";
+import PrimaryButton from "../../../components/PrimaryButton/PrimaryButton";
+import googlePlay from "../../../assets/images/banner/google-play.png";
+import appStore from "../../../assets/images/banner/app-store.png";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>This is Banner</h2>
+    <div className="banner-area">
+      <Container>
+        <div className="banner-content">
+          <h2>Your Favorite Food Delivery Partner</h2>
+          <p>
+            The food at your doorstep. Why starve when you have us. You hunger
+            partner. Straight out of the oven to your doorstep.{" "}
+          </p>
+          <div className="search-area">
+            <div className="search-text">
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Enter your delivery location"
+              />
+            </div>
+            <div className="search-btn">
+              <PrimaryButton buttonText="order now" to="/"></PrimaryButton>
+            </div>
+          </div>
+          <div className="app-download">
+            <img src={appStore} alt="" />
+            <img src={googlePlay} alt="" className="google-play" />
+          </div>
+        </div>
+        <div className="banner-img">
+          <img src={bannerImg} alt="" />
+        </div>
+      </Container>
     </div>
   );
 };
